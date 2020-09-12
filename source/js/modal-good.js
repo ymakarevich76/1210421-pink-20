235de6e2fa47cada5edf8222fa498282208f5577
YuliaKa <ymakarevich76@gmail.com>
2020-09-12 17:47:55 +0300

    Correct pages after check

--- source/js/modal-good.js
+++ source/js/modal-good.js
@@ -4,22 +4,22 @@ var modalClose = document.querySelector('.modal__button');
 
 for(var i = 0; i < modalLink.length; i++) {
 
-modalLink[i].addEventListener('click', function(evt) {
-evt.preventDefault();
-modalGood.classList.add('modal__show');
-});
+  modalLink[i].addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modalGood.classList.add('modal__show');
+  });
 }
 
 modalClose.addEventListener('click', function(evt) {
-evt.preventDefault();
-modalGood.classList.remove('modal__show');
+  evt.preventDefault();
+  modalGood.classList.remove('modal__show');
 });
 
 window.addEventListener('keydown', function(evt) {
-if(evt.keyCode===27) {
-if(modalGood.classList.contains('modal__show')) {
-evt.preventDefault();
-modalGood.classList.remove('modal__show');
-}
-}
+  if(evt.keyCode===27) {
+    if(modalGood.classList.contains('modal__show')) {
+    evt.preventDefault();
+    modalGood.classList.remove('modal__show');
+    }
+  }
 });
