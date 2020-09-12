5bdcc3e1d7d2086fef8a09b8096a42296cccb977
YuliaKa <ymakarevich76@gmail.com>
2020-09-12 23:19:56 +0300

    Correct error-modal

--- source/js/modal-error.js
+++ source/js/modal-error.js
@@ -4,22 +4,22 @@ var popupClose = document.querySelector('.modal__btn');
 
 for (var i = 0; i < popupLink.length; i++) {
 
-popupLink[i].addEventListener('click', function(evt) {
-evt.preventDefault();
-modalError.classList.add('modal__show');
-});
+  popupLink[i].addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modalError.classList.add('modal__show');
+  });
 }
 
 popupClose.addEventListener('click', function(evt) {
-evt.preventDefault();
-modalError.classList.remove('modal__show');
+  evt.preventDefault();
+  modalError.classList.remove('modal__show');
 });
 
 window.addEventListener('keydown', function(evt) {
-if(evt.keyCode===27) {
-if(modalError.classList.contains('modal__show')) {
-evt.preventDefault();
-modalError.classList.remove('modal__show');
-}
-}
+  if(evt.keyCode===27) {
+    if(modalError.classList.contains('modal__show')) {
+    evt.preventDefault();
+    modalError.classList.remove('modal__show');
+    }
+  }
 });
